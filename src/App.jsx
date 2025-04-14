9e2c52888b9b4109990b4285701c9dd12800545e
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-14 17:17:17 +0200

    feat: add imputs and event function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,74 @@
 import { useState } from 'react'
 
 
+
 function App() {
+
+  const [formPost, setFormPost] = useState({
+    author: '',
+    title: '',
+    body: '',
+    public: false
+  })
+
+  function handleFormPost(e) {
+
+    const valueCheckbox = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+
+    setFormPost(formPost => ({
+      ...formPost,
+      [e.target.name]: e.target.value
+    }))
+
+  }
 
   return (
     <>
+      <h1>React Post Form</h1>
+      <div>
+        <label htmlFor="">Nome dell'autore </label>
+        <input type="text"
+          id="author-name"
+          name="author"
+          value={formPost.author}
+          onChange={handleFormPost}
+          placeholder='Nome autore' />
+      </div>
 
+      <hr />
+
+      <div>
+        <label htmlFor="">Titolo del post </label>
+        <input type="text"
+          id="title-post"
+          name="title"
+          value={formPost.title}
+          onChange={handleFormPost}
+          placeholder='Titolo del post' />
+      </div>
+
+      <hr />
+
+      <div>
+        <label htmlFor="">Contenuto del post </label>
+        <input type="text"
+          id="body-post"
+          name="body"
+          value={formPost.body}
+          onChange={handleFormPost}
+          placeholder='Contenuto del post' />
+      </div>
+
+      <hr />
+
+      <div>
+        <label htmlFor="">Il post è pubblico? </label>
+        <input type="checkbox"
+          id="public-post"
+          name="public"
+          checked={formPost.public}
+          onChange={handleFormPost} />
+      </div>
     </>
   )
 }
